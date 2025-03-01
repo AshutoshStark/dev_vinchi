e8bf0e7a9ffcef313df48ae11d4e1894adafb9c1
Ashutosh Kumar <ashu28jan2003@gmail.com>
2025-03-01 17:15:13 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
       initial="hidden"
       animate="visible"
       className="flex justify-center items-center w-full h-auto md:h-screen bg-top md:bg-center bg-contain md:bg-cover bg-no-repeat overflow-hidden bg-[url(../public/assets/images/bgImage.svg)] ">
-      <motion.div variants={slideInFromTop} className="mt-40 md:mt-6 flex gao-6 md:gap-12 flex-col md:flex-row bg-emerald-300 px-5 md:px-12 py-6 rounded-lg w-full md:max-w-3/4 bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-40">
+      <motion.div variants={slideInFromTop} className="mt-40 md:mt-6 flex gao-6 md:gap-12 flex-col md:flex-row bg-emerald-300 px-5 md:px-12 py-6 rounded-lg w-full md:max-w-3/5 bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-40">
          {/* for mobile images */}
          <div className="flex flex-row justify-between items-center -mt-28 md:hidden">
             <motion.button onClick={()=>imageSwiper(false)} className="bg-white backdrop-blur-lg rounded-full p-1.5">
@@ -53,22 +53,22 @@ export default function Home() {
           <img src="assets/images/review.svg" alt="review" className="w-1/4" />
           <motion.p
           variants={slideInFromLeft(0.5)}
-           className="text-2xl font-semibold pt-3">
+           className="text-3xl font-semibold pt-3">
           Small warehouse space for rent in <span className="text-[#00AA6C]"> Upper West Side, Atlanta, GA</span>
           </motion.p>
           {/* description */}
           <div className="flex flex-col gap-4 py-3">
             <motion.div variants={slideInFromLeft(0.6)} className="flex items-center gap-3">
-              <img src="assets/icons/calander.svg" alt="" className="w-1/20"/>
-              <p className="font-semibold text-[10px]">Flexible spaces & leases <br /> <samp className="font-medium">Leases as short as 6 months. Upscale or downscale anytime!</samp></p>
+              <img src="assets/icons/calander.svg" alt="" className="w-1/15"/>
+              <p className="font-semibold text-xs">Flexible spaces & leases <br /> <samp className="font-medium">Leases as short as 6 months. Upscale or downscale anytime!</samp></p>
             </motion.div>
             <motion.div variants={slideInFromLeft(0.7)} className="flex items-center gap-3">
-              <img src="assets/icons/wareHouse.svg" alt="" className="w-1/20"/>
-              <p className="font-semibold text-[10px]">Flexible spaces & leases <br /> <samp className="font-medium">Leases as short as 6 months. Upscale or downscale anytime!</samp></p>
+              <img src="assets/icons/wareHouse.svg" alt="" className="w-1/15"/>
+              <p className="font-semibold text-xs">Flexible spaces & leases <br /> <samp className="font-medium">Leases as short as 6 months. Upscale or downscale anytime!</samp></p>
             </motion.div>
             <motion.div variants={slideInFromLeft(0.8)} className="flex items-center gap-3">
-              <img src="assets/icons/key.svg" alt="" className="w-1/20"/>
-              <p className="font-semibold text-[10px]">Flexible spaces & leases <br /> <samp className="font-medium">Leases as short as 6 months. Upscale or downscale anytime!</samp></p>
+              <img src="assets/icons/key.svg" alt="" className="w-1/15"/>
+              <p className="font-semibold text-xs">Flexible spaces & leases <br /> <samp className="font-medium">Leases as short as 6 months. Upscale or downscale anytime!</samp></p>
             </motion.div>
           </div>
           {/* images */}
@@ -80,7 +80,7 @@ export default function Home() {
           </div>
           {/* area description */}
           <div className="py-3">
-            <motion.p variants={slideInFromLeft(0.9)} className="text-[#00AA6c] text-xs">200-2 border-gray-600
+            <motion.p variants={slideInFromLeft(0.9)} className="text-[#00AA6c] text-sm">200-2 border-gray-600
               ,000 sq ft starting as low as $625/month</motion.p>
           </div>
         </div>
@@ -88,7 +88,7 @@ export default function Home() {
           <motion.p variants={slideInFromRight(0.6)} className="text-base font-semibold">Visit for an in-person tour</motion.p>
           <motion.div variants={slideInFromRight(0.7)} className="flex">
             <img src="assets/images/formImage.svg" alt="image" className="w-1/5 " />
-            <p className="text-[10px]">Cashana or Khadijah will answer your questions and find you a space that meets your needs. No commitment necessary!</p>
+            <p className="text-sm">Cashana or Khadijah will answer your questions and find you a space that meets your needs. No commitment necessary!</p>
           </motion.div>
           <div className="w-full gap-2 flex flex-col justify-center items-center py-4">
             <input type="text" placeholder="First Name" className="border-2 border-gray-300 text-xs w-full rounded-sm p-2"/>
@@ -97,8 +97,8 @@ export default function Home() {
             <input type="text" placeholder="Phone number (Just so we can remind you of your appt)" className="border-2 border-gray-300 text-xs w-full rounded-sm p-2"/>
           </div>
           <motion.button variants={slideInFromRight(0.7)}className='w-full text-xs font-semibold text-white bg-[#00AA6C] px-6 py-2 rounded-lg shadow-m shadow-slate-400 flex items-center justify-center'>Step 2: Find a time that works <span><FaAngleRight /></span></motion.button>
-          <motion.p variants={slideInFromRight(0.7)} className="text-[10px] py-2 pb-8">Questions? Give us a call: <span className="text-[#00AA6C]">415-985-0940</span></motion.p>
-          <motion.button variants={slideInFromRight(0.7)} className=' text-[9px] text-black bg-emerald-100 px-3 py-1 rounded-full shadow-m shadow-slate-400 flex items-center justify-center'>Looking for a job? <span className="underline">Click here.</span></motion.button>
+          <motion.p variants={slideInFromRight(0.7)} className="text-sm py-2 pb-8">Questions? Give us a call: <span className="text-[#00AA6C]">415-985-0940</span></motion.p>
+          <motion.button variants={slideInFromRight(0.7)} className=' text-sm text-black bg-emerald-100 px-3 py-1 rounded-full shadow-m shadow-slate-400 flex items-center justify-center'>Looking for a job? <span className="underline">Click here.</span></motion.button>
         </motion.div>
       </motion.div>
     </motion.div>
